dce23b5b5918795f556124a876cec21b60b311a3
James Kuligowski <jdkuligowski@gmail.com>
2022-05-04 12:12:33 +0100

    filtered added to categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -10,6 +10,10 @@ const Categories = () => {
   const [categories, setCategories] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setErrors] = useState(false)
+  const [filteredCategories, setFilteredCategories] = useState([])
+  const [filters, setFilters] = useState({
+    searchTerm: ''
+  })
 
   useEffect(() => {
     const getCategories = async () => {
@@ -26,24 +30,48 @@ const Categories = () => {
     getCategories()
   }, [])
 
+  const handleChange = (e) => {
+    const updatedObj = {
+      ...filters,
+      [e.target.name]: e.target.value
+    }
+    setFilters(updatedObj)
+    console.log(updatedObj)
+  }
+
+  useEffect(() => {
+    if (categories.length) {
+      const regexSearch = new RegExp(filters.searchTerm, 'i')
+      const filtered = categories.filter(category => {
+        return regexSearch.test(category.strCategory)
+      })
+      console.log('filtered', filtered)
+      setFilteredCategories(filtered)
+    }
+  }, [filters, categories])
+
+
   return (
     <section className='category-container'>
       <h1>Categories</h1>
+      <div className="filter-container">
+        <input type="text" name="searchTerm" placeholder='Search...' value={filters.searchTerm} onChange={handleChange} />
+      </div>
       <div className="category-content">
         {loading ? <Spinner />
           : error ? <p>error</p>
             : !categories ? <PageNotFound /> :
-            categories.map(category => {
-              const { strCategory, idCategory, strCategoryThumb } = category
-              return (
-                <Link to={`/category/${strCategory}`} key={idCategory}>
-                  <div className='category-tile'>
-                    <div className='category-title'>{strCategory}</div>
-                    <img src={strCategoryThumb} alt='category' />
-                  </div>
-                </Link>
-              )
-            })}
+              filteredCategories.map(category => {
+                const { strCategory, idCategory, strCategoryThumb } = category
+                return (
+                  <Link to={`/category/${strCategory}`} key={idCategory}>
+                    <div className='category-tile'>
+                      <div className='category-title'>{strCategory}</div>
+                      <img src={strCategoryThumb} alt='category' />
+                    </div>
+                  </Link>
+                )
+              })}
       </div>
     </section>
   )
